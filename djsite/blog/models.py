a43c645932360524a2805bf7a5add5b1a756c1ba
PauloGuillen <psm.guillen@gmail.com>
2023-04-27 19:36:23 -0300

    Criado a lista e o detalhe do blog

--- djsite/blog/models.py
+++ djsite/blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManage(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManage, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -17,9 +23,19 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager()
+    published = PublishedManage()
+    
     class Meta:
         ordering = ('-publish',)
     
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', 
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug
+                             ])
